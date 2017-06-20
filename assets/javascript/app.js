39e458a21b572164f9a0daa8ea57ec652834c1a3
Michael Farrelly <farrellyt@Marikos-MacBook-Pro.local>
2017-06-20 18:37:30 -0400

    the logic behind right/wrong answers doesnt totally work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,133 +1,180 @@
-// var panel = $
-
-
 $(document).ready(function(){
-// stating variables and functions to be used in game
-var allQuestions = [
-		{
-			question: "Ben’s General Store is selling yarn for $.04 a foot. How many feet can you buy for $0.52?" + '<br>', 
-			possibleAnswersMC: ["14 feet", "10 feet", "15 feet", "8 feet", "12 feet"],
-			correctAnswer: "13 feet", 
-		},
-		{
-			question: "Are the following two words similar, contradictory, or not related?" + '<br>' + "Legend, Key" + '<br>', 
-			possibleAnswersMC: ["similar", "contradictory", "not related"],
-			correctAnswer: "similar", 
-		},
-		{
-			question: "Which of these numbers is the smallest?" + '<br>',
-			possibleAnswersMC: ["0.24", "0.11", "0", "2", "0.02"], 
-			correctAnswer: "0", 
-		},
-		{
-			question: "A girl is 21 years old and her brother is a third her age. When the girl is 36, what will be the age of her brother?" + '<br>',
-			possibleAnswersMC: ["6", "3", "7", "5", "4"], 
-			correctAnswer: "2", 
-		},
-		{
-			question: "(15 ÷ 5) x (10 ÷ 2) =" + '<br>',
-			possibleAnswersMC: ["15", "10", "2.5", "5", "13"], 
-			correctAnswer: "15", 
-		},
-		{
-			question: "Are the following two words similar, contradictory, or not related?" + "<br>" + "Familiar, Gallant" + '<br>',
-			possibleAnswersMC: ["similar", "contradictory", "not related"], 
-			correctAnswer: "not related", 
-		},
-		{
-			question: "-57 + -74 =" + '<br>', 
-			possibleAnswersMC: ["-137", "-17", "17", "-121", "-131"],
-			correctAnswer: "-131", 
-		},
-		{
-			question: "Over the course of two weeks, Al spent $105.00 on lunch. What was the average cost per day?" + '<br>', 
-			possibleAnswersMC: ["$6.50", "$7.50", "$8.05", "$7.25", "$5.75"],
-			correctAnswer: "$7.50", 
-		},
-		{
-			question: "Reduce the fraction 21/49 to its lowest terms." + '<br>', 
-			possibleAnswersMC: ["5/7", "3/8", "6/9", '21/49'],
-			correctAnswer: "3/7", 
-		},
-		{
-			question: "A copy machine repairman makes $12.50 per hour plus $4 for every service call he performs. Last week he worked 34 hours and made 6 service calls. How much money did he make?" + '<br>',
-			possibleAnswersMC: ["400", "375", "440", "420", "438"], 
-			correctAnswer: "449", 
-		}];
-	var trivia = {
-		numCorrect: 0,
-		numIncorrect: 0,
-		wScore: 0,
-		questions: [],
-		possibleAnswers: [],
-		userAnswer: "",
-		counter: 140,
 
-		// countdown: function() {
-		// 	tivia.counter--;
-		// 	$('#timeLeft').html(trivia.counter);
-		// 	if (trivia.counter === 0) {
-		// 		console.log("Times Up!");
-		// 		trivia.timeUp();
-		// 	timer = setInterval(trivia.countdown, 1000);
-		// 	}
-		// },
+// setting viewer equal to test id on html
+// making the counter 144 for Wonderlic test
+var viewer = $('#test');
+var countStartNumber = 14;
 
+// click events here
+// click event to reset game
+$(document).on('click', '#start-over', function(e) {
+  game.reset();
+});
 
-		// timeUp: function() {
-		// 	clearInterval(timer)
-		// },
-		// list all questions and answers
-		// the possibleAnswersMC will show up as multiple choice
-		
-		startGame: function() {
-			// run a loop through allQuestions to populate printedQuestions and printedPossibleAnswers
-			for (var i = 0; i < allQuestions.length; i++) {
-				trivia.questions.push(allQuestions[i].question);
-				trivia.possibleAnswers.push(allQuestions[i].possibleAnswersMC);
-				}
-			},
-		// results: function() {
-		// 	clearInterval(timer);
-		// 	$('#afterMath').html('<h2>All done, heres how you did!</h2>');
-		//     $('#counter-number').html(trivia.counter);
-		//     $('#afterMath').append('<h3>Correct Answers: ' + trivia.correct + '</h3>');
-		//     $('#afterMath').append('<h3>Incorrect Answers: ' + trivia.incorrect + '</h3>');
-		//     $('#afterMath').append('<h3>Unanswered: ' + (questions.length - (trivia.incorrect + trivia.correct)) + '</h3>');
-		//     $('#afterMath').append('<br><button id="start-over">Start Over?</button>');
-		// }
-		// clicked: function(e) {
-  //   clearInterval(timer);
+//click answer to guess that answer 
+$(document).on('click', '.answer-button', function(e) {
+  game.clicked(e);
+});
 
-  //   if ($(e.target).data("name") === questions[this.questions].correctAnswer){
-  //     	this.answeredCorrectly();
-  //   	} 
-  //   else {
-  //     		this.answeredIncorrectly();
-  //   	}
- //  		},
-	};
-
-
-// When button is clicked, run startGame function
-// $('button').click(startGame());
-trivia.startGame();
-console.log(trivia.questions);
-console.log(trivia.possibleAnswers);
-$('#printedQuestions').html(trivia.questions);
-$('#printedPossibleAnswersMC').html(trivia.possibleAnswers);
-
+// when start button clicked, add timer and question to screen
+$(document).on('click', '#start', function(e) {
+  $('#subwrapper').prepend('<h2>Time Left: <span id="counter-number">14</span> Seconds</h2>');
+  game.loadQuestion();
 });
 
 
-// print the values in respective html locations
 
+// variables and functions to be used in game
+// object of questions
+var questions = [
+		{
+			question: "Ben's General Store is selling yarn for $.04 a foot. How many feet can you buy for $0.52?", 
+			answers: ["13 feet", "10 feet", "15 feet", "8 feet", "12 feet"],
+			correctAnswer: "13 feet", 
+		},
+		{
+			question: "Are the following two words similar, contradictory, or not related? Legend, Key", 
+			answers: ["similar", "contradictory", "not related"],
+			correctAnswer: "similar", 
+		},
+		{
+			question: "Which of these numbers is the smallest?",
+			answers: ["0.24", "0.11", "0", "2", "0.02"], 
+			correctAnswer: "0", 
+		},
+		{
+			question: "A girl is 21 years old and her brother is a third her age. When the girl is 36, what will be the age of her brother?",
+			answers: ["6", "3", "7", "5", "2"], 
+			correctAnswer: "2", 
+		},
+		{
+			question: "(15 ÷ 5) x (10 ÷ 2) =",
+			answers: ["15", "10", "2.5", "5", "13"], 
+			correctAnswer: "15", 
+		},
+		{
+			question: "Are the following two words similar, contradictory, or not related? Familiar, Gallant",
+			answers: ["similar", "contradictory", "not related"], 
+			correctAnswer: "not related", 
+		},
+		{
+			question: "-57 + -74 =", 
+			answers: ["-137", "-17", "17", "-121", "-131"],
+			correctAnswer: "-131", 
+		},
+		{
+			question: "Over the course of two weeks, Al spent $105.00 on lunch. What was the average cost per day?", 
+			answers: ["$6.50", "$7.50", "$8.05", "$7.50", "$5.75"],
+			correctAnswer: "$7.50", 
+		},
+		{
+			question: "Reduce the fraction 21/49 to its lowest terms.", 
+			answers: ["5/7", "3/7", "6/9", '21/49'],
+			correctAnswer: "3/7", 
+		},
+		{
+			question: "A copy machine repairman makes $12.50 per hour plus $4 for every service call he performs. Last week he worked 34 hours and made 6 service calls. How much money did he make?",
+			answers: ["400", "375", "449", "420", "438"], 
+			correctAnswer: "449", 
+		}];
+	var game = {
+		currentQuestion: 0,
+		correct: 0,
+		incorrect: 0,
+		// wScore: 0,
+		questions: questions,
+		counter: countStartNumber,
 
+// function to show timer counting down, and defines what happens when the timer hits 0
+	    countdown: function() {
+			game.counter--;
+		    $('#counter-number').html(game.counter);
 
+		    if (game.counter === 0){
+		      console.log('TIME UP');
+		      game.timeUp();
+		    }
+		},
 
+// function to reset timer, load question on screen and the buttons for answers
+		loadQuestion: function(){
+		    timer = setInterval(game.countdown, 1000);
+		    viewer.html('<h2>' + questions[this.currentQuestion].question + '</h2>' );
+		    for (var i = 0; i<questions[this.currentQuestion].answers.length; i++){
+		    	viewer.append('<button class="answer-button" id="button"' + 'data-name="' + questions[this.currentQuestion].answers[i] + '">' + questions[this.currentQuestion].answers[i]+ '</button>');
+		    }
+		},
+// function to reset clock, go to next question and load that question plus answers
+		nextQuestion: function(){
+		    game.counter = countStartNumber;
+		    $('#counter-number').html(game.counter);
+		    game.currentQuestion++;
+		    game.loadQuestion();
+		},
+// function to say what happens when time is up
+		timeUp: function() {
+  			clearInterval(timer);
+    		$('#counter-number').html(game.counter);
+    		viewer.html('<h2>Out of Time!</h2>');
+    		viewer.append('<h3>The Correct Answer was: ' + questions[this.currentQuestion].correctAnswer);
+    			if (game.currentQuestion === questions.length - 1){
+      				setTimeout(game.results, 3 * 1000);
+    			} else {
+      				setTimeout(game.nextQuestion, 3 * 1000);
+    			}
+  		},		
+// function to print all info about your performance
+    	results: function() {
+    		clearInterval(timer);
+		    $('#counter-number').html(game.counter);
+		    viewer.append('<h3>Correct Answers: ' + game.correct + '</h3>');
+		    viewer.append('<h3>Incorrect Answers: ' + game.incorrect + '</h3>');
+		    viewer.append('<h3>Unanswered: ' + (questions.length - (game.incorrect + game.correct)) + '</h3>');
+		    viewer.append('<br><button id="start-over">Start Over?</button>');
+  		},
+// to check if answer is correct or nah
+  		clicked: function(e) {
+	    clearInterval(timer);
 
+	    if ($(e.target).data("name") === questions[this.currentQuestion].correctAnswer){
+	      this.answeredCorrectly();
+	    } else {
+	      this.answeredIncorrectly();
+	    }
+	  	},
+// function if incorrect answer is chosen
+		answeredIncorrectly: function() {
+			game.incorrect++;
+		    clearInterval(timer);
+		    viewer.html('<h2>Wrong!</h2>');
+		    viewer.append('<h3>The Correct Answer was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
 
+		    if (game.currentQuestion === questions.length - 1){
+		      setTimeout(game.results, 3 * 1000);
+		    } else {
+		      setTimeout(game.nextQuestion, 3 * 1000);
+		    }
+		},
+// function if correct answer is chosen
+		answeredCorrectly: function(){
+		    clearInterval(timer);
+		    game.correct++;
+		    viewer.html('<h2>Right!</h2>');
 
-
-
+		    if (game.currentQuestion === questions.length - 1){
+		      setTimeout(game.results, 3 * 1000);
+		    } else {
+		      setTimeout(game.nextQuestion, 3 * 1000);
+		    }
+		  },
+// reset function to start game over
+		reset: function(){
+		    this.currentQuestion = 0;
+		    this.counter = countStartNumber;
+		    this.correct = 0;
+		    this.incorrect = 0;
+		    this.loadQuestion();
+		  }
+		};
+});
 
